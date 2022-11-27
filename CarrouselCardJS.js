9d517c0e87c4ba30ef719696c6ee6a654fba1f35
LoghannV <loghann2000@gmail.com>
2022-11-27 13:03:21 -0300

    Atualizacao

--- CarrouselCardJS.js
+++ CarrouselCardJS.js
@@ -1,15 +1,24 @@
+//Declaracao das variaveis e constates
 let btn_next = document.querySelector('.carousel-box-next')
 let btn_prev = document.querySelector('.carousel-box-prev')
 let CarouselOne = document.getElementById("CarouselOne")
 let BoxGroup = document.querySelector('.box-group')
+let CardGroupCarousel = document.querySelector('.card-group-carousel')
+let Cards = document.querySelectorAll('.card')
+const cssCarouselOne = window.getComputedStyle(CarouselOne)
+const cssGroupCard = window.getComputedStyle(CardGroupCarousel)
 
+//Atribuição das funções
 window.addEventListener('load',function(){
     let alturaCarouselOne = CarouselOne.style.top
     btn_next.style.top = alturaCarouselOne+"px"
     btn_prev.style.top = alturaCarouselOne+"px"
     btn_next.style.left = (window.innerWidth-100)+"px"
+    btn_next.style.height = cssCarouselOne.height
+    btn_prev.style.height = cssCarouselOne.height
     BoxGroup.style.top = alturaCarouselOne+"px"
     BoxGroup.style.width = this.window.innerWidth+"px"
+    CardGroupCarousel.style.width = Cards.length*260+"px"
 })
 
 let proximo = 0
